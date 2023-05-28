2846b75b277d4269958049ca2a144ca100fe7b4d
Stefan Krastanov <stefan@krastanov.org>
2023-05-28 15:59:24 -0400

    update tests and CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,31 @@
+using SafeTestsets
 using QuantumInterface
-using Test
 
-@testset "QuantumInterface.jl" begin
-    # Write your tests here.
+function doset(descr)
+    if length(ARGS) == 0
+        return true
+    end
+    for a in ARGS
+        if occursin(lowercase(a), lowercase(descr))
+            return true
+        end
+    end
+    return false
 end
+
+macro doset(descr)
+    quote
+        if doset($descr)
+            @safetestset $descr begin
+                include("test_"*$descr*".jl")
+            end
+        end
+    end
+end
+
+println("Starting tests with $(Threads.nthreads()) threads out of `Sys.CPU_THREADS = $(Sys.CPU_THREADS)`...")
+
+@doset "sortedindices"
+#VERSION >= v"1.9" && @doset "doctests"
+get(ENV,"JET_TEST","")=="true" && @doset "jet"
+VERSION >= v"1.9" && @doset "aqua"
