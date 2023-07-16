cd247d8b098ef98dde6ec0c1aa8f9d2b67b56e3e
Stefan Krastanov <github.acc@krastanov.org>
2023-07-16 18:17:13 -0400

    declare identitysuperoperator (#16)

--- src/sparse.jl
+++ src/sparse.jl
@@ -9,7 +9,7 @@ Convert an arbitrary operator into a [`SparseOperator`](@ref).
 """
 sparse(a::AbstractOperator) = throw(ArgumentError("Direct conversion from $(typeof(a)) not implemented. Use sparse(full(op)) from QuantumOptics instead."))
 
-function ptrace(x::AbstractSparseMatrix, shape_nd, indices)
+function ptrace(x::AbstractSparseMatrix, shape_nd, indices) # A fairly QuantumOptics-specific method, left here to avoid piracy
     shape_nd = (shape_nd...,)
     N = div(length(shape_nd), 2)
     shape_2d = (x.m, x.n)
@@ -17,18 +17,19 @@ function ptrace(x::AbstractSparseMatrix, shape_nd, indices)
     shape_2d_after = (prod(shape_nd_after[1:N]), prod(shape_nd_after[N+1:end]))
     I_nd_after_max = CartesianIndex(shape_nd_after...)
     y = spzeros(eltype(x), shape_2d_after...)
-    for I in eachindex(x)
-        I_nd = sub2sub(shape_2d, shape_nd, I)
+    for I in eachindex(x)::CartesianIndices # Manual type assertions to help JET
+        println(I.I)
+        I_nd = sub2sub(shape_2d, shape_nd, I)::CartesianIndex
         if I_nd.I[indices] != I_nd.I[indices .+ N]
             continue
         end
-        I_after = sub2sub(shape_nd_after, shape_2d_after, min(I_nd, I_nd_after_max))
+        I_after = sub2sub(shape_nd_after, shape_2d_after, min(I_nd, I_nd_after_max)::CartesianIndex)
         y[I_after] += x[I]
     end
     y
 end
 
-function sub2sub(shape1, shape2, I)
+function sub2sub(shape1, shape2, I::CartesianIndex)::CartesianIndex # Manual type assertions to help JET
     linearindex = LinearIndices(shape1)[I.I...]
     CartesianIndices(shape2)[linearindex]
 end
