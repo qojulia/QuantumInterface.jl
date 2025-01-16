1adf939ddb6703da970dc0d3fda8bbf05140d53f
apkille <killeandrew@gmail.com>
2025-01-16 12:33:08 -0500

    update docstring and add `AbstractUse` method

--- src/express.jl
+++ src/express.jl
@@ -1,6 +1,15 @@
+"""
+    express(obj, repr::AbstractRepresentation)
+    express(obj, repr::AbstractRepresentation, use::AbstractUse)
+
+Translate a quantum object `obj` to a backend representation `repr`. It is relevant to define `use`
+for formalism-specific cases, e.g., for `QuantumCliffordRepr`.
+"""
 function express end
 
 express(obj) = express(obj, QuantumOpticsRepr()) # The default representation
+express(s::Number, repr::AbstractRepresentation, use::AbstractUse) = s
+express(s, repr::AbstractRepresentation) = express(s, repr, UseAsState())
 
 """An abstract type for the supported representation of quantum objects."""
 abstract type AbstractRepresentation end
