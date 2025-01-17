66290c0df2eb0c1d75b8e4c5e4b6065a8994c2ab
Stefan Krastanov <github.acc@krastanov.org>
2025-01-17 16:55:55 -0500

    Simplify the JET static analysis setup following aviatesk/JET.jl#532 (#47)

--- test/test_jet.jl
+++ test/test_jet.jl
@@ -2,46 +2,21 @@ using QuantumInterface
 using Test
 using JET
 
-using JET: ReportPass, BasicPass, InferenceErrorReport, UncaughtExceptionReport, MethodErrorReport
-
-# Custom report pass that ignores `UncaughtExceptionReport`
-# Too coarse currently, but it serves to ignore the various
-# "may throw" messages for runtime errors we raise on purpose
-# (mostly on malformed user input)
-struct MayThrowIsOk <: ReportPass end
+using JET: ReportPass, DefinitionAnalysisPass, BasicPass, InferenceErrorReport, UncaughtExceptionReport, MethodErrorReport
 
 # We define abstract methods, so it is unsurprising that there are no matching methods unless
 # we import other libraries.
 # TODO find a more fine-grained way to check these.
 struct NoMatchingMethodIsOK <: ReportPass end
 
-# ignores `UncaughtExceptionReport` analyzed by `JETAnalyzer`
-(::MayThrowIsOk)(::Type{UncaughtExceptionReport}, @nospecialize(_...)) = return
-
-# forward to `BasicPass` for everything else
-function (::MayThrowIsOk)(report_type::Type{<:InferenceErrorReport}, @nospecialize(args...))
-    BasicPass()(report_type, args...)
-end
-
-# ignores `UncaughtExceptionReport` and `MethodErrorReport` analyzed by `JETAnalyzer`
+# ignores `MethodErrorReport` analyzed by `JETAnalyzer`
 (::NoMatchingMethodIsOK)(::Type{MethodErrorReport}, @nospecialize(_...)) = return
 
-# forward to `MayThrowIsOk` for everything else
 function (::NoMatchingMethodIsOK)(report_type::Type{<:InferenceErrorReport}, @nospecialize(args...))
-    MayThrowIsOk()(report_type, args...)
+    DefinitionAnalysisPass()(report_type, args...) # Using it instead of BasePass, see https://github.com/aviatesk/JET.jl/pull/532
 end
 
 @testset "JET checks" begin
-    rep = report_package("QuantumInterface";
-        report_pass=MayThrowIsOk(),
-        ignored_modules=(
-            #AnyFrameModule(...),
-        )
-    )
-    @show rep
-    @test length(JET.get_reports(rep)) <= 11
-    @test_broken length(JET.get_reports(rep)) == 0
-
     rep = report_package("QuantumInterface";
         report_pass=NoMatchingMethodIsOK(),
         ignored_modules=(
