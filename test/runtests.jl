b807e34c1a53367fec4bc63790fa1812df2411ca
Dennis Wayo <117969019+DennisWayo@users.noreply.github.com>
2026-06-11 13:45:58 +0500

    Add lazy option to QuantumOpticsRepr

--- test/runtests.jl
+++ test/runtests.jl
@@ -27,6 +27,7 @@ println("Starting tests with $(Threads.nthreads()) threads out of `Sys.CPU_THREA
 
 @doset "sortedindices"
 @doset "bases"
+@doset "express"
 #VERSION >= v"1.9" && @doset "doctests"
 get(ENV,"JET_TEST","")=="true" && @doset "jet"
 VERSION >= v"1.9" && @doset "aqua"
